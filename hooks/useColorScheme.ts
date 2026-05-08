0ce35afa4017f56f108e639bee4f5d4e47c75241
Zain Gill <zaingill45@gmail.com>
2026-05-08 11:04:07 -0600

    Project reset to native android

--- hooks/useColorScheme.ts
+++
@@ -1,8 +0,0 @@
-import { useColorScheme as useRNColorScheme } from 'react-native';
-
-export function useColorScheme(): 'light' | 'dark' | null {
-  const scheme = useRNColorScheme();
-  if (scheme === 'dark') return 'dark';
-  if (scheme === 'light') return 'light';
-  return null;
-}
